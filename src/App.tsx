babca09e96a616e02a51c305f5641d6105e2390d
Willem Verbuyst <willemverbuyst@gmail.com>
2023-06-01 04:53:53 +0200

    Add lexical

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import PlaintTextEditor from "./Editors/PlainTextEditor";
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <h1>Lexical</h1>
+      <PlaintTextEditor />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Editors/PlainTextEditor.tsx
@@ -0,0 +1,46 @@
+import { $getRoot, $getSelection, EditorState } from "lexical";
+
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+
+const theme = {};
+
+function onChange(editorState: EditorState) {
+  editorState.read(() => {
+    // Read the contents of the EditorState here.
+    const root = $getRoot();
+    const selection = $getSelection();
+
+    console.log(root, selection);
+  });
+}
+
+function onError(error: Error) {
+  console.error(error);
+}
+
+function PlaintTextEditor() {
+  const initialConfig = {
+    namespace: "MyEditor",
+    theme,
+    onError,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <OnChangePlugin onChange={onChange} />
+      <HistoryPlugin />
+    </LexicalComposer>
+  );
+}
+
+export default PlaintTextEditor;
