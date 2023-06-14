a9d68fa19b35d15cce3ec167ade0b126dfe7e897
Willem Verbuyst <willemverbuyst@gmail.com>
2023-06-14 04:25:17 +0200

    Add link

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,15 @@ import Editor from "./Editor";
 function App() {
   return (
     <div className="container">
+      <div className="link">
+        <a
+          href="https://github.com/willemverbuyst/lexical-rte"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="link__icon icon__github" />
+        </a>
+      </div>
       <h1 className="gradient-text">lexical rte</h1>
       <div className="editor-wrapper">
         <Editor />
